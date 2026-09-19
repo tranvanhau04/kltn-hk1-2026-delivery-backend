157c6f3b26d95055dafc3cebfe8ca5a34dfb3a3e
Tran Van Hau <vanhautran592@gmail.com>
2026-09-19 20:28:54 +0700

    build: update dist after merge

--- dist/database/database.module.js
+++ dist/database/database.module.js
@@ -18,6 +18,7 @@ const tracking_log_entity_1 = require("../entities/tracking-log.entity");
 const user_entity_1 = require("../entities/user.entity");
 const zone_entity_1 = require("../entities/zone.entity");
 const zone_driver_entity_1 = require("../entities/zone-driver.entity");
+const password_reset_entity_1 = require("../entities/password-reset.entity");
 let DatabaseModule = class DatabaseModule {
 };
 exports.DatabaseModule = DatabaseModule;
@@ -31,10 +32,11 @@ exports.DatabaseModule = DatabaseModule = __decorate([
                 username: 'root',
                 password: 'sapassword',
                 database: 'delivery_db',
-                entities: [depot_entity_1.Depot, order_entity_1.Order, driver_entity_1.Driver, route_entity_1.Route, stop_entity_1.Stop, tracking_log_entity_1.TrackingLog, user_entity_1.User, zone_entity_1.Zone, zone_driver_entity_1.ZoneDriver],
+                entities: [depot_entity_1.Depot, order_entity_1.Order, driver_entity_1.Driver, route_entity_1.Route, stop_entity_1.Stop, tracking_log_entity_1.TrackingLog, user_entity_1.User, zone_entity_1.Zone, zone_driver_entity_1.ZoneDriver, password_reset_entity_1.PasswordReset],
                 synchronize: false,
                 logging: false,
                 charset: 'utf8mb4_unicode_ci',
+                timezone: 'Z',
                 extra: {
                     charset: 'utf8mb4_unicode_ci',
                 },
